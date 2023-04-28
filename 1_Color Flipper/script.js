aa809f0c6d33b5400f2928184807616f88b64963
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-04-28 15:45:06 +0530

    done

--- 1_Color Flipper/script.js
+++ 1_Color Flipper/script.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+const button = document.querySelector("button")
+const body = document.querySelector("body")
+const color = ["red", "pink", "green", "orange", "white", "gray", "#fff4", "blue", "yellow", "#f4f5", "brown" ]
+
+// body.style.backgroundColor = aquamarine;
+
+button.addEventListener("click", changeBg)
+
+function changeBg(){
+    const colorIndex = parseInt(Math.random()*color.length)
+    body.style.backgroundColor = color[colorIndex]
+}
+
---
+++ 2_Login Page/script.js
@@ -0,0 +1,17 @@
+
+
+function myFunction(){
+const user = document.getElementById("uname").value
+const pwd = document.getElementById("pwd").value
+
+
+
+
+    if(user == "admin" && pwd == "admin"){
+        document.write("Login successfully")
+    }
+    else{
+        window.alert("Check Your Credentials")
+    }
+}
+    
